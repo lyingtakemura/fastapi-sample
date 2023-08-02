b12c3248db45f0190b77e2bbd1b6165c2af2ba93
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-08-02 16:32:15 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "test"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
