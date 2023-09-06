13d97a297330379ae02cd10c9d6a08ceff3c84fa
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-09-06 18:43:01 +0300

    Use sessionmaker in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 from main import app, get_db
 from models import Base
@@ -9,9 +9,7 @@ from models import Base
 
 @pytest.fixture(scope="session")
 def engine():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}
-    )
+    engine = create_engine("sqlite://", connect_args={"check_same_thread": False})
     Base.metadata.create_all(bind=engine)
     yield engine
 
@@ -20,7 +18,7 @@ def engine():
 def db(engine):
     with engine.connect() as connection:
         connection.begin()
-        session = Session(bind=connection)
+        session = sessionmaker(autocommit=False, autoflush=False, bind=connection)()
         yield session
         session.rollback()
 
