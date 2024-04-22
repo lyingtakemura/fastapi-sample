92e6e123a3a8dc7ffeaf9115b27e10373b800a16
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2024-04-22 19:33:59 +0300

    add error handling for posts endpoints #6

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, status
-from sqlalchemy import select
+from fastapi import APIRouter, HTTPException, status
+from sqlalchemy import exc, select
 from sqlalchemy.orm import Session
 
 from config.database import engine
@@ -15,11 +15,50 @@ async def select_all():
         return [row.Post.__dict__ for row in session.execute(select(Post)).all()]
 
 
+@url.get("/{id}", response_model=PostOutSchema, status_code=status.HTTP_200_OK)
+async def select_one(id):
+    with Session(engine) as session:
+        try:
+            statement = select(Post).filter_by(id=id)
+            obj = session.execute(statement).scalar_one()
+            return obj.__dict__
+        except exc.NoResultFound:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
 @url.post("/", response_model=PostOutSchema, status_code=status.HTTP_201_CREATED)
-async def insert(payload: PostInSchema):
+async def create_post(payload: PostInSchema):
     with Session(engine) as session:
         obj = Post(**payload.model_dump())
         session.add(obj)
         session.commit()
         session.refresh(obj)
         return obj.__dict__
+
+
+@url.put("/{id}", response_model=PostOutSchema, status_code=status.HTTP_200_OK)
+async def update_post(id: int, title: str, body: str):
+    with Session(engine) as session:
+        try:
+            statement = select(Post).filter_by(id=id)
+            obj = session.execute(statement).scalar_one()
+            obj.title = title
+            obj.body = body
+            session.commit()
+            session.refresh(obj)
+            return obj.__dict__
+        except exc.NoResultFound:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@url.delete("/{id}", response_model=PostOutSchema, status_code=status.HTTP_200_OK)
+async def delete_post(id: int):
+    with Session(engine) as session:
+        try:
+            statement = select(Post).filter_by(id=id)
+            obj = session.execute(statement).scalar_one()
+            session.delete(obj)
+            session.commit()
+            return obj.__dict__
+        except exc.NoResultFound:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
